ad685f766363230275cdcbb5aa9b7dc591fa0571
freekysachin <freekysachin06@gmail.com>
2025-07-01 12:53:44 +0530

    contact card tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ContactForm from './components/contact-form/ContactForm';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ContactForm />
     </div>
   );
 }
--- frontend/src/components/contact-form/ContactForm.js
+++ frontend/src/components/contact-form/ContactForm.js
@@ -2,15 +2,21 @@ import React from 'react'
 
 function ContactForm() {
   return (
-    <div>
-      <div></div>
-      <div>
-        <input placeholder='Full Name' />
-        <input placeholder='Enter Email Address'/>
-        <input placeholder='Mobile Number'/>
-        <input placeholder='Area, City'/>
+    <div className='flex flex-col max-w-md px-10 gap-8 border py-6 bg-[#3F5574] text-white'>
+      <div className='text-center text-4xl font-bold '>
+        <h1>Get a Free Consultation</h1>
       </div>
-      <button className='bg-orange-500'>Get Quick Quote</button>
+      <div className='flex flex-col gap-4'>
+        <input placeholder='Full Name'
+            className='border rounded-md p-2 bg-transparent placeholder:font-bold placeholder:text-white focus:outline-none' />
+        <input placeholder='Enter Email Address' 
+            className='border rounded-md p-2 bg-transparent placeholder:font-bold placeholder:text-white focus:outline-none'/>
+        <input placeholder='Mobile Number' 
+            className='border rounded-md p-2 bg-transparent placeholder:font-bold placeholder:text-white focus:outline-none'/>
+        <input placeholder='Area, City' 
+            className='border rounded-md p-2 bg-transparent placeholder:font-bold placeholder:text-white focus:outline-none'/>
+      </div>
+      <button className='bg-orange-500 block p-2 rounded-md text-lg font-bold'>Get Quick Quote</button>
     </div>
   )
 }
