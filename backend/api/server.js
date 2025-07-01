74fe9b7280d0cca939b423eef8eb8206349daead
freekysachin <freekysachin06@gmail.com>
2025-07-01 21:12:58 +0530

    Going ServerLess

---
+++ backend/api/server.js
@@ -0,0 +1,33 @@
+// const multer = require('multer');
+// const {v2: cloudinary} = require('cloudinary');
+
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+
+const express = require('express');
+require('dotenv').config();
+const dbConnect = require('../db/connection.js');
+const adminRoutes = require('../routes/adminRoutes.js');
+const projectRoutes = require('../routes/projectRoutes.js')
+const clientRoutes = require('../routes/clientRoutes.js')
+const subscribeRoutes = require('../routes/subscribeRoutes.js')
+const formRoutes = require('../routes/contactForm.js')
+var cors = require('cors')
+
+const serverless = require('serverless-http');
+
+
+dbConnect()
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.use('/api', projectRoutes);
+app.use('/api', clientRoutes);
+app.use('/api', subscribeRoutes)
+app.use('/api',formRoutes )
+// app.use('api/admin', adminRoutes);
+
+module.exports = app;
+module.exports.handler = serverless(app)
